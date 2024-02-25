ab6b55991af6616125c3dd85f0b33f7c51b342a2
parvinderBabar <er.kaurparvinder@gmail.com>
2024-02-24 20:14:55 -0500

    commit

---
+++ register.js
@@ -0,0 +1,7 @@
+<script>
+      document
+        .getElementById('registerButton')
+        .addEventListener('click', function () {
+          document.getElementById('signupForm').style.display = 'block'
+        })
+    </script>
