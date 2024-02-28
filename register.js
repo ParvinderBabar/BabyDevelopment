59469c99e2adb675bba5ebc7b3f5d7a8bf65dbd3
parvinderBabar <er.kaurparvinder@gmail.com>
2024-02-28 04:01:51 -0500

    commit

--- register.js
+++ register.js
@@ -1,20 +1,21 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-
-
-
-function redirectToLogin() {
-       // Retrieve values from input fields
+    // Retrieve values from input fields
     const email = document.getElementById('InputEmail').value;
-    const inputName = document.getElementById('InputName').value;
+    const inputName = document.getElementById('InputName');
     const password = document.getElementById('InputPassword').value;
-console.log(email);
-console.log(password);
-console.log(inputName);
-        
-        // Construct the URL with the name parameter
-        var loginUrl = "login.html?name=" + encodeURIComponent(inputName);
- 
-        // Redirect the user to the login page with the name parameter
-        window.location.href = loginUrl;
+
+    if (!inputName.checkValidity()) {
+        // If the input is invalid, prevent form submission
+        alert("Please enter a valid name containing only letters and up to 20 characters.");
+        return; // Exit the function early if validation fails
     }
-    
+
+    // Construct the URL with the name parameter
+    var loginUrl = "login.html?name=" + encodeURIComponent(inputName.value);
+
+    // Redirect the user to the login page with the name parameter
+    window.location.href = loginUrl;
+});
